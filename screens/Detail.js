804ca0d648d944b1c3368869ed29efded0544d33
kimgostring <kimgostring@naver.com>
2022-10-26 17:10:43 +0900

    [Add] AppListModule 추가 모듈의 getAppList 함수를 통해, 설치 앱 이름, 아이콘 비트맵 리스트를 리턴받을 수 있음

--- screens/Detail.js
+++ screens/Detail.js
@@ -2,11 +2,19 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import {NativeBaseProvider, Center, Button} from 'native-base';
 
+import AppListModule from '../wrap_module';
+
 function Detail({navigation}) {
+  const onPressAppList = async () => {
+    const {nameList, iconList} = await AppListModule.getAppList();
+    console.log('7번째 앱: ' + nameList[7]);
+    console.log(iconList[7]);
+  };
+
   return (
     <NativeBaseProvider style={styles.v}>
       <Center flex={1}>
-        <Button onPress={() => {}}>BLANK</Button>
+        <Button onPress={onPressAppList}>Detail</Button>
       </Center>
     </NativeBaseProvider>
   );
