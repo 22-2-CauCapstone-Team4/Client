9192bcc78174efc80e3f3773a0453df6edba6750
Urchinode <vrdhan212@gmail.com>
2022-10-28 00:22:59 +0900

    [Update] 로그인 UI 수정

--- screens/CreateAccountScreen.js
+++ screens/CreateAccountScreen.js
@@ -23,6 +23,7 @@ function CreateAccountScreen({navigation}) {
   const [emailValid, setEmailValid] = React.useState('');
   const [passwordValid, setPasswordValid] = React.useState('');
   const [confirmValid, setConfirmValid] = React.useState('');
+  const [createValid, setCreateValid] = React.useState('');
 
   const {signUp, signIn} = useAuth();
 
@@ -38,6 +39,7 @@ function CreateAccountScreen({navigation}) {
 
   const onPressSignUp = async () => {
     if (emailValid === ' ' && passwordValid === ' ' && confirmValid === ' ') {
+      setCreateValid('');
       try {
         await signUp(email, password);
         await signIn(email, password);
@@ -54,120 +56,129 @@ function CreateAccountScreen({navigation}) {
           duration: SnackBar.LENGTH_SHORT,
         });
       }
+    } else {
+      setCreateValid('필요한 정보를 모두 입력해주세요.');
     }
   };
 
   return (
     <NativeBaseProvider>
-      <VStack my="20" flex={1} alignItems="center">
-        <Box w="300px" alignItems="center">
-          <VStack>
-            <Text mb="2" fontSize="20" fontWeight="700">
-              회원 가입
-            </Text>
-          </VStack>
-          <Divider
+      <Center flex={1} alignItems="center">
+        <VStack>
+          <Box w="300px" alignItems="center">
+            <VStack>
+              <Text mb="8" fontSize="3xl" fontWeight="700">
+                회원 가입
+              </Text>
+            </VStack>
+            {/* <Divider
             mb="10"
             _light={{
               bg: 'muted.400',
-            }}></Divider>
-        </Box>
+            }}></Divider> */}
+          </Box>
 
-        <Box w="280px">
-          <HStack
-            w="280px"
-            alignItems="center"
-            justifyContent="space-between"
-            maxW="500px"
-            maxH="100px">
-            <Input
-              onChangeText={setEmail}
-              value={email}
-              w="180px"
-              h="45px"
-              size="md"
-              mr="3"
-              placeholder="이메일"
-            />
-            <Button
-              bg="#0891b220"
-              _text={{color: 'primary.600'}}
-              variant={'active'}
-              onPress={() => {
-                if (!regexEmail.test(email)) {
-                  setEmailValid('올바른 형식이 아닙니다.');
-                } else {
-                  setEmailValid(' ');
-                }
-              }}>
-              확인
-            </Button>
-          </HStack>
-          <Text mb="1" fontSize="xs" color="#DD0000">
-            {emailValid}
-          </Text>
-          <VStack w="280px" justifyContent="center">
-            <Input
-              secureTextEntry={true}
-              onChangeText={text => {
-                setPassword(text);
-                comparePasswords(text);
-                if (text.length < 8) {
-                  setPasswordValid('비밀번호는 8자리 이상이어야 합니다.');
-                } else {
-                  setPasswordValid(' ');
-                }
-              }}
-              value={password} // 입력하면, 입력 이메일 안 보이게 수정하기
-              w="180px"
-              size="md"
-              placeholder="비밀번호"
-            />
-            <Text mb="1" fontSize="xs" color="#DD0000">
-              {passwordValid}
+          <Box w="300px" justifyItems="center" mb="6">
+            <HStack
+              w="300px"
+              justifyContent="space-between"
+              alignItems="center"
+              maxW="500px"
+              maxH="100px">
+              <Input
+                onChangeText={setEmail}
+                value={email}
+                w="200px"
+                h="45px"
+                size="md"
+                mr="3"
+                placeholder="이메일"
+              />
+              <Button
+                w="80px"
+                bg="#0891b220"
+                _text={{color: 'primary.600'}}
+                onPress={() => {
+                  if (!regexEmail.test(email)) {
+                    setEmailValid('올바른 형식이 아닙니다.');
+                  } else {
+                    setEmailValid(' ');
+                  }
+                }}>
+                확인
+              </Button>
+            </HStack>
+            <Text my="1" fontSize="xs" color="#DD0000">
+              {emailValid}
             </Text>
-            <Input
-              secureTextEntry={true}
-              onChangeText={text => {
-                setConfirm(text);
-                if (text !== password) {
-                  setConfirmValid('비밀번호가 동일하지 않습니다.');
-                } else {
-                  setConfirmValid(' ');
-                }
-              }}
-              value={confirm} // 입력하면, 입력 이메일 안 보이게 수정하기
-              w="180px"
-              size="md"
-              placeholder="비밀번호 확인"
-            />
-            <Text mb="1" fontSize="xs" color="#DD0000">
-              {confirmValid}
-            </Text>
-          </VStack>
-          <HStack
-            mb="7"
-            alignItems="center"
-            justifyContent="space-between"
-            w="280px">
-            <Input w="180px" h="45px" size="md" mr="3" placeholder="닉네임" />
-            <Button
-              bg="#0891b220"
-              _text={{color: 'primary.600'}}
-              onPress={() => {}}>
-              확인
+            <VStack w="300px" justifyContent="center">
+              <Input
+                secureTextEntry={true}
+                onChangeText={text => {
+                  setPassword(text);
+                  comparePasswords(text);
+                  if (text.length < 8) {
+                    setPasswordValid('비밀번호는 8자리 이상이어야 합니다.');
+                  } else {
+                    setPasswordValid(' ');
+                  }
+                }}
+                value={password} // 입력하면, 입력 이메일 안 보이게 수정하기
+                w="300px"
+                size="md"
+                placeholder="비밀번호"
+              />
+              <Text my="1" fontSize="xs" color="#DD0000">
+                {passwordValid}
+              </Text>
+              <Input
+                secureTextEntry={true}
+                onChangeText={text => {
+                  setConfirm(text);
+                  if (text !== password) {
+                    setConfirmValid('비밀번호가 동일하지 않습니다.');
+                  } else {
+                    setConfirmValid(' ');
+                  }
+                }}
+                value={confirm} // 입력하면, 입력 이메일 안 보이게 수정하기
+                w="300px"
+                size="md"
+                placeholder="비밀번호 확인"
+              />
+              <Text my="1" fontSize="xs" color="#DD0000">
+                {confirmValid}
+              </Text>
+            </VStack>
+            <HStack
+              justifyContent="space-between"
+              alignItems="center"
+              w="300px">
+              <Input w="200px" h="45px" size="md" mr="3" placeholder="닉네임" />
+              <Button
+                w="80px"
+                bg="#0891b220"
+                _text={{color: 'primary.600'}}
+                onPress={() => {}}>
+                확인
+              </Button>
+            </HStack>
+          </Box>
+          <Box>
+            <Pressable onPress={() => navigation.pop()}>
+              <Text textAlign="right" mb="2">
+                로그인하기
+              </Text>
+            </Pressable>
+            <Button mb="2" w="300px" onPress={onPressSignUp}>
+              계정 생성
             </Button>
-          </HStack>
-        </Box>
-        <Box alignItems="center">
-          <Pressable onPress={() => navigation.pop()}>
-            <Text mb="5">로그인 화면으로 전환</Text>
-          </Pressable>
-          <Button w="200px" onPress={onPressSignUp}>
-            계정 생성
-          </Button>
-        </Box>
-      </VStack>
+            <Text textAlign="center" color="#DD0000">
+              {createValid}
+            </Text>
+          </Box>
+        </VStack>
+      </Center>
     </NativeBaseProvider>
   );
 }
--- screens/LogInScreen.js
+++ screens/LogInScreen.js
@@ -15,36 +15,37 @@ import SnackBar from 'react-native-snackbar';
 function LogInScreen({navigation}) {
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [loginValid, setLoginValid] = React.useState(' ');
 
   const {signIn} = useAuth();
 
   const onPressSignIn = async () => {
     try {
       await signIn(email, password);
+      setLoginValid(' ');
       navigation.navigate('Detail');
-
       SnackBar.show({
         text: '로그인이 완료되었습니다. ',
         duration: SnackBar.LENGTH_SHORT,
       });
     } catch (err) {
       console.error(err.message);
-      SnackBar.show({
-        text: `로그인에 실패했습니다. : ${err.message}`,
-        duration: SnackBar.LENGTH_SHORT,
-      });
+      setLoginValid('이메일 또는 비밀번호를 다시 확인해주세요.');
     }
   };
 
   return (
     <NativeBaseProvider>
       <Center flex={1}>
-        <Circle size="100px" bg="secondary.400" mb="10" />
+        {/* <Circle size="100px" bg="secondary.400" mb="10" /> */}
+        <Text alignSelf="center" fontSize="3xl" fontWeight="700" mb="8">
+          로그인
+        </Text>
         <Box>
           <Input
             onChangeText={setEmail}
             value={email}
-            w="200px"
+            w="300px"
             m="1"
             size="md"
             placeholder="이메일"
@@ -53,23 +54,25 @@ function LogInScreen({navigation}) {
             onChangeText={text => setPassword(text)}
             value={password}
             secureTextEntry={true} // 입력하면, 입력 이메일 안 보이게 수정하기
-            w="200px"
+            w="300px"
             m="1"
-            mb="4"
+            mb="6"
             size="md"
             placeholder="비밀번호"
           />
           <Pressable
-            alignSelf="flex-end"
+            w="300px"
             onPress={() => navigation.navigate('계정 만들기')}>
-            <Text>가입하기</Text>
+            <Text textAlign="right">가입하기</Text>
           </Pressable>
         </Box>
 
-        {/*로그인 성공 시 Detail(가명) 네비로 이동 */}
-        <Button m="10" onPress={onPressSignIn}>
+        <Button w="300px" mt="2" mb="2" onPress={onPressSignIn}>
           로그인
         </Button>
+        <Text fontSize="sm" color="#DD0000" alignSelf="center">
+          {loginValid}
+        </Text>
       </Center>
     </NativeBaseProvider>
   );
