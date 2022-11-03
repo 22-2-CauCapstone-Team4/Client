d4212613705f9861a7ddaa7dea1f827bf1051bdd
kimgostring <kimgostring@naver.com>
2022-11-03 17:35:32 +0900

    [Add] 모듈에서 JS로 이벤트 보내기 기능 추가

--- android/app/src/main/java/com/sub/CheckAppService.java
+++ android/app/src/main/java/com/sub/CheckAppService.java
@@ -3,17 +3,28 @@ package com.sub;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.LongSparseArray;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+import com.facebook.react.HeadlessJsTaskService;
+
 public class CheckAppService extends Service {
     private static final int SERVICE_NOTIFICATION_ID = 0315;
     private static final String CHANNEL_ID = "PHONELOCK";
+
+    NotificationManager notificationManager;
+    Notification notification;
 
     @Nullable
     @Override
@@ -24,14 +35,30 @@ public class CheckAppService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         createNotificationChannel(); // Creating channel for API 26+
-        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE);
+
+        notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 // 실제 알림창에 뜨는 내용 설정
                 .setContentTitle("감지 중")
                 .setContentText("금지 앱 접속을 감지 중입니다. ")
                 .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(contentIntent)
                 .build();
 
         startForeground(SERVICE_NOTIFICATION_ID, notification);
+
+        // 스레드 생성해서 이 위치에서 시작시켜야 함
+        // intent 하나 보내보기
+        Intent checkAppIntent = new Intent(getApplicationContext(), CheckAppEventService.class);
+        Bundle bundle = new Bundle();
+
+        bundle.putString("msg", "안녕하세요!");
+        checkAppIntent.putExtras(bundle);
+
+        getApplicationContext().startService(checkAppIntent);
+
         return START_STICKY;
     }
 
@@ -41,7 +68,7 @@ public class CheckAppService extends Service {
             // 애플리케이션 -> 설정 -> 알림 카테고리에 뜨는 이름, 설명 설정
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "PHONE LOCK", importance);
             channel.setDescription("금지 앱 접속 확인 기능이 종료되지 않고 계속 수행되기 위해 필요한 알림입니다. ");
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
     }
--- index.js
+++ index.js
@@ -6,4 +6,9 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+const AppCheckHeadlessTask = async taskData => {
+  console.log('CheckApp 이벤트를 받았습니다. ', taskData);
+};
+
+AppRegistry.registerHeadlessTask('CheckApp', () => AppCheckHeadlessTask);
 AppRegistry.registerComponent(appName, () => App);
