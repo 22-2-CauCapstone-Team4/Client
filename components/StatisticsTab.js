7790864821a31f9cdf648424c43e10cb71660207
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2022-11-06 18:41:45 +0900

    [Add]react-native-chart-kit

--- components/StatisticsTab.js
+++ components/StatisticsTab.js
@@ -1,6 +1,16 @@
 import Categories from './Categories';
 import React from 'react';
 import styled from 'styled-components/native';
+import {Text, ScrollView, TouchableOpacity, View} from 'react-native';
+import {Dimensions} from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
 
 const Container = styled.View`
   height: 100%;
@@ -8,9 +18,45 @@ const Container = styled.View`
   padding: 20px;
 `;
 const StatisticsTab = () => {
+  const screenWidth = Dimensions.get('window').width;
+
+  const data = {
+    labels: ['미션 중', '포기 중', '평소'],
+    legend: ['Dataset1', 'Dataset2'],
+    data: [
+      [60, 60],
+      [30, 30],
+      [30, 30],
+    ],
+    barColors: ['#fe6383', '#36a2eb'],
+  };
   return (
     <Container>
       <Categories />
+      <ScrollView>
+        <Text>StackedBar chart</Text>
+        <StackedBarChart
+          data={data}
+          width={screenWidth}
+          height={220}
+          chartConfig={{
+            backgroundColor: '#e26a00',
+            backgroundGradientFrom: '#fb8c00',
+            backgroundGradientTo: '#ffa726',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: '6',
+              strokeWidth: '2',
+              stroke: '#ffa726',
+            },
+          }}
+        />
+      </ScrollView>
     </Container>
   );
 };
