d020352734fb87f9ef878e03a268eb3bb97f502c
Urchinode <vrdhan212@gmail.com>
2022-11-09 04:50:52 +0900

    [Fix] 금지 앱 이름 너무 길면 배치 깨지는 현상 해결

--- components/screen/BlockAppScreen.js
+++ components/screen/BlockAppScreen.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {
   SafeAreaView,
@@ -16,14 +16,14 @@ const numColumns = 3;
 export const apps = [
   {id: 0, appName: 'Chrome'},
   {id: 1, appName: 'YouTube'},
-  {id: 2, appName: '카카오톡'},
+  {id: 2, appName: 'GIF 메이커-편집기'},
   {id: 3, appName: 'Instagram'},
-  {id: 4, appName: 'Chrome'},
-  {id: 5, appName: 'YouTube'},
+  {id: 4, appName: 'Microsoft Launcher'},
+  {id: 5, appName: 'LearningXStudent'},
   {id: 6, appName: '카카오톡'},
   {id: 7, appName: 'Instagram'},
-  {id: 8, appName: 'Chrome'},
-  {id: 9, appName: 'YouTube'},
+  {id: 8, appName: 'QR 코드 스캐너'},
+  {id: 9, appName: 'Samsung Global Goals'},
   {id: 10, appName: '카카오톡'},
   {id: 11, appName: 'Instagram'},
   {id: 12, appName: 'Chrome'},
@@ -38,7 +38,7 @@ export const apps = [
 
 // blockedApps: 해당 유저가 설정해놓은 금지 앱 정보들
 
-export default function BlockApp({navigation, onCreate}) {
+export default function BlockApp({navigation}) {
   const [blockedApps, setBlockedApps] = useState([
     // 사용자가 설정해 둔 금지 어플 여기다가 저장
     {id: 0, appName: 'Chrome'},
@@ -46,14 +46,14 @@ export default function BlockApp({navigation, onCreate}) {
     {id: 2, appName: '카카오톡'},
   ]);
 
-  const [clicked, setClicked] = useState(true);
-
-  const clickedText = () => <Text>123</Text>;
-
   //FlatList에 어플 하나씩 나열
   _renderItems = ({item, index}) => {
     return (
-      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+      <View
+        style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
         <TouchableOpacity
           style={styled.appBox}
           onPress={() => {
@@ -61,13 +61,11 @@ export default function BlockApp({navigation, onCreate}) {
             if (blockedApps.find(item => item.id === index)) {
               console.log('now is free');
               setBlockedApps(blockedApps.filter(app => app.id !== index));
-              setClicked(false);
             }
             // 없으면 blockedApp에 추가하고 클릭 표시 아이콘 state 변경
             else {
               console.log('now is blocked');
               setBlockedApps(blockedApps.concat(item));
-              setClicked(true);
             }
           }}>
           {blockedApps.find(item => item.id === index) ? (
@@ -78,7 +76,12 @@ export default function BlockApp({navigation, onCreate}) {
             />
           ) : null}
         </TouchableOpacity>
-        <Text style={{color: 'black'}}>{item.appName}</Text>
+        {/*앱 박스 너비, 높이 달라지면 width 변경해줘야함*/}
+        <View style={{width: 80}}>
+          <Text numberOfLines={1} style={{color: 'black'}}>
+            {item.appName}
+          </Text>
+        </View>
       </View>
     );
   };
