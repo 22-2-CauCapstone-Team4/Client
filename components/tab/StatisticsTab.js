80459e4bf491281b0b3ef46f5c75900d5debe820
zenoo97 <85465356+zenoo97@users.noreply.github.com>
2022-11-17 02:50:15 +0900

    [Update] 통계탭 - 차트추가

--- components/tab/StatisticsTab.js
+++ components/tab/StatisticsTab.js
@@ -1,17 +1,9 @@
-import Categories from '../Categories';
 import React from 'react';
 import styled from 'styled-components/native';
 import {Text, ScrollView, TouchableOpacity, View} from 'react-native';
 import {Dimensions} from 'react-native';
 import Colors from '../../utils/Colors';
-import {
-  LineChart,
-  BarChart,
-  PieChart,
-  ProgressChart,
-  ContributionGraph,
-  StackedBarChart,
-} from 'react-native-chart-kit';
+import {StackedBarChart, BarChart} from 'react-native-chart-kit';
 
 const Container = styled.View`
   height: 100%;
@@ -21,21 +13,44 @@ const Container = styled.View`
 const StatisticsTab = () => {
   const screenWidth = Dimensions.get('window').width;
 
-  const data = {
-    labels: ['미션 중', '포기 중', '평소'],
+  // 1번쨰 차트
+  const data1 = {
+    labels: ['미션 중', '포기 중', '평소'], // ★ 레이블
+    legend: ['데이터1', '데이터2'], // ★ 파란색, 빨간색 바를 나타내는 내용 작성하면 됌
     data: [
-      [60, 60],
+      // ★ 데이터 넣는 곳([a, b] => a = 빨간색 바, b = 파란색 바)
+      [30, 60],
       [30, 30],
       [30, 30],
     ],
     barColors: ['#fe6383', '#36a2eb'],
   };
+  // 2번 째 차트
+  const data2 = {
+    labels: ['일', '월', '화', '수', '목', '금', '토'], // ★ 내용 수정
+    datasets: [
+      // ★ 데이터 넣는 곳
+      {
+        data: [1, 2, 3, 4, 5, 6, 7],
+      },
+    ],
+  };
+  const chartConfig = {
+    backgroundGradientFrom: '#ffffff',
+    backgroundGradientFromOpacity: 0.1,
+    backgroundGradientTo: '#ffffff',
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 0.5) => `rgb(0, 0, 0)`,
+    barPercentage: 0.5,
+  };
   return (
     <Container>
       <ScrollView>
-        <Text>chart1</Text>
+        <Text style={{color: 'black', fontSize: 20, textAlign: 'center'}}>
+          chart1
+        </Text>
         <StackedBarChart
-          data={data}
+          data={data1}
           width={screenWidth * 0.95}
           height={220}
           chartConfig={{
@@ -53,6 +68,17 @@ const StatisticsTab = () => {
               stroke: '#ffa726',
             },
           }}
+        />
+        <Text style={{color: 'black', fontSize: 20, textAlign: 'center'}}>
+          chart2
+        </Text>
+        <BarChart
+          withHorizontalLabels
+          data={data2}
+          fromZero
+          width={screenWidth * 0.95}
+          height={220}
+          chartConfig={chartConfig}
         />
       </ScrollView>
     </Container>
